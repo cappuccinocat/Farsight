f4f861c2407944ee7fe810b675b3f3c851204f2c
Andrew Schreiber <schreiaj@gmail.com>
2016-06-30 19:42:58 -0400

    Add sliders

--- app/src/main/java/com/nutrons/farsight/farsight/CameraActivity.java
+++ app/src/main/java/com/nutrons/farsight/farsight/CameraActivity.java
@@ -2,8 +2,10 @@ package com.nutrons.farsight.farsight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.SeekBar;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -13,8 +15,10 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,7 +34,9 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
     double[] colorLow;
     double[] colorHigh;
     private CameraBridgeViewBase cameraView;
+    Mat mRgba, mRgbaF, mRgbaT;
 
+    SeekBar hBar, sBar, vBar;
     BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 
         @Override
@@ -60,6 +66,10 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        hBar = (SeekBar)findViewById(R.id.hBar);
+        sBar = (SeekBar)findViewById(R.id.sBar);
+        vBar = (SeekBar)findViewById(R.id.vBar);
+
 
         cameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
         cameraView.setCvCameraViewListener(this);
@@ -76,6 +86,9 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
         hsv = new Mat(height, width, CvType.CV_8U);
         mask = new Mat(height, width, CvType.CV_8U);
         hierarchy = new Mat(height, width, CvType.CV_8U);
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
         CONTOUR_COLOR = new Scalar(255, 0, 0, 255);
     }
 
@@ -88,9 +101,31 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        getProcessedMat(inputFrame.rgba(), colorLow, colorHigh);
+//        getProcessedMat(inputFrame.rgba(), colorLow, colorHigh);
+        mRgba = inputFrame.rgba();
+        Core.transpose(mRgba, mRgbaT);
+        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0, 0, 0);
+        Core.flip(mRgbaF, mRgba, 1);
+        getProcessedMat(mRgba, hBar.getProgress(), sBar.getProgress(), vBar.getProgress());
         getLargestContour(mask);
         Imgproc.drawContours(mask, largestContour, -1, CONTOUR_COLOR);
+        if(largestContour.size() >0 ){
+            MatOfPoint targ = largestContour.get(0);
+            Moments p = Imgproc.moments(targ);
+            int cx = (int) (p.get_m10() / p.get_m00());
+            int cy = (int) (p.get_m01() / p.get_m00());
+            Imgproc.circle(mask, new Point(cx, cy), 10, new Scalar(255, 49, 255));
+        }
+
+        return mask;
+    }
+
+
+    public Mat getProcessedMat(Mat image, double hVal, double sVal, double vVal){
+        Imgproc.cvtColor(image, hsv, Imgproc.COLOR_BGR2HSV);
+        Scalar lThresh = new Scalar(new double[]{hVal-20, sVal, vVal});
+        Scalar hThresh = new Scalar(new double[]{hVal+20, 255, 255});
+        Core.inRange(hsv, lThresh, hThresh, mask);
         return mask;
     }
 
@@ -104,7 +139,7 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 
     public List<MatOfPoint> getLargestContour(Mat threshedImage){
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Imgproc.findContours(threshedImage, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(threshedImage, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         
         double maxArea = 0;
         Iterator<MatOfPoint> each = contours.iterator();
